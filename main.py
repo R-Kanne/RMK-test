4aeed36dd0e9f7371884a863603af89eb0a83a3c
Raino <rainokanne74@gmail.com>
2025-05-21 11:06:33 +0300

    Created initial simulate_journey function

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
+# Time by which Rita must arrive, in seconds from midnight (9:05 AM).
+MEETING_TIME = 9 * 3600 + 5 * 60
 
-def simulate_journey(departure_time: int) -> bool   :
+
+def generate_bus_ride_time():
+    """
+    Returns how long the bus drives in seconds.
+    """
+
+def generate_wait_for_bus():
+    """
+    Returns how long Rita has to wait for the bus in seconds
+    """
+
+
+
+def simulate_single_journey(departure_time: int, ) -> bool   :
     """
     Simulate a single journey starting at `departure_time`.
     Returns True if Rita is late, False otherwise.
     """
+    to_bus_stop = 300   # Seconds
+    wait_for_bus = generate_wait_for_bus()  # Seconds
+    bus_ride_time = generate_bus_ride_time()    # Seconds
+    from_bus_to_meeting = 240   # Seconds
+
+    total_trip_time = to_bus_stop + wait_for_bus + bus_ride_time + from_bus_to_meeting  # Seconds
+    arrival_time = departure_time + total_trip_time    # Seconds
+
+    return arrival_time > MEETING_TIME
+
+
+
 
 
 def simulations_per_departure() -> float:
@@ -30,15 +57,6 @@ def main():
     Main entry point: sets parameters, calls simulation functions,
     outputs or plots results.
     """
-
-
-
-
-
-
-
-
-
 
 
 
