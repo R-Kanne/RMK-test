a8dd44e91058d741bd4ea2a67f67ef5c8a09524d
Raino <rainokanne74@gmail.com>
2025-05-21 10:24:25 +0300

    Improved the overall structure logic

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-def simulate_journey(departure_time: int):
+
+def simulate_journey(departure_time: int) -> bool   :
     """
     Simulate a single journey starting at `departure_time`.
     Returns True if Rita is late, False otherwise.
     """
 
 
-def many_simulations():
+def simulations_per_departure() -> float:
     """
-    Runs many simulations over a range of departure times,
-    collects lateness probabilities and returns structured results.
+    Runs many simulations over a single departure time.
+    Returns the probability of running late as a float based on many runs.
     """
 
-
+def run_simulation_over_range(start_time, end_time, step_minutes, num_simulations) -> List[Tuple[time, probability]]:
+    """
+    Loops over departure times from start_time to end_time in increments of step_minutes.
+    For each departure time calls simulation_per_departure function.
+    Returns a list of tuples containing each departure times respective simulated probability.
+    """
 
 
 
